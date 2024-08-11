c4a6025e4425692a4aa72ffa9b82a828446b4b9b
Ronald Stiven Nastacuas Gallardo <ronald.nastacuas@vasslatam.com>
2024-08-11 00:08:27 -0500

    Update connection DB

--- database/config.js
+++ database/config.js
@@ -6,8 +6,8 @@ const dbConnection = async() => {
     try {
         
         await mongoose.connect(process.env.DB_CNN,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
         });
 
         console.log("DB ONLINE")
